93984b7c7b4f5c55d0dcc44594c3dd8be349540f
Yerim <yerimchoi115@gmail.com>
2021-01-19 18:25:13 +0900

    13rd try

--- baseball_game.py
+++ baseball_game.py
@@ -58,7 +58,7 @@ def is_between_100_and_999(user_input_number):
     # '''
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당
-    if not is_digit(three_digit):
+    if not is_digit(user_input_number):
         return True
     result=0
     digit=len(user_input_number)-1
